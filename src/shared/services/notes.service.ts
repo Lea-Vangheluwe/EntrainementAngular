61a2e4673611358436fe1424f249e6bee7ba3fb2
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-10-20 14:57:29 +0200

    ajout du service note

--- src/shared/services/notes.service.ts
+++ src/shared/services/notes.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+notes: Note[] = [];
+
+  getData(): Observable<Note[]> {
+      return this.httpClient.get<Note[]>(`${environment.serverUrl}/notes/`)
+    }
+
+ //Suppression d'une note
+  deleteData(idToDelete: number){
+    let url = `${environment.serverUrl}/notes/${idToDelete}`;
+    return this.httpClient.delete(url, this.httpOptions)
+  }
+
+  addData(note: Note): Observable<Note> {
+      return this.httpClient.post<Note>(`${environment.serverUrl}/notes/`, note, this.httpOptions)
+  }
+
+  editNote(note: Note){
+    return this.httpClient.put<Note>(`${environment.serverUrl}/notes/${note.id}`, note, this.httpOptions)
+  }
+
+  constructor(private httpClient: HttpClient) { }
 }
