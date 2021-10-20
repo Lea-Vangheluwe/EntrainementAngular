d5e3c1d292354ce43ab83df53c7bc1b992843ccc
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-10-20 16:48:24 +0200

    Ajout note fonctionnel

--- src/app/ajouter-note/ajouter-note.component.ts
+++ src/app/ajouter-note/ajouter-note.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-ajouter-note',
   templateUrl: './ajouter-note.component.html',
-  styleUrls: ['./ajouter-note.component.scss']
+  styleUrls: ['./ajouter-note.component.scss'],
+  providers: [NgbModalConfig, NgbModal]
 })
 export class AjouterNoteComponent implements OnInit {
+  @Output() addDataEvent = new EventEmitter<string>();
+  ajoutNoteForm = new FormGroup({
+    content: new FormControl('')
+  });
+  placeHolder: string = 'Ajoutez votre donnée ici'
 
-  constructor() { }
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
 
   ngOnInit(): void {
+  }
+
+  addData(){
+    this.addDataEvent.emit(this.ajoutNoteForm.value.content);
+    this.ajoutNoteForm.patchValue({content: ''})
+  }
+
+  open(content: any) {
+    this.modalService.open(content);
   }
 
 }
--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -9,15 +9,31 @@ import { NotesService } from '../shared/services/notes.service';
 })
 export class NotesComponent implements OnInit {
   notes: Note[] = [];
-
+  note: Note = {
+    id: 0,
+    content: ''
+  }
 
   constructor(private notesService : NotesService) {  }
+
+  addNote(content: string) {
+    if (content != '') {
+      this.note = {
+        id: Date.now(),
+        content: content
+      };
+      this.notesService.addData(this.note).subscribe(addNote => this.notes.push(this.note))
+    }
+    else {
+      window.alert ('Merci d\'ajouter une donnée !')
+    }
+  };
 
   deleteNote(idToDelete: number){
     this.notesService.deleteData(idToDelete).subscribe(
       DeleteNote => this.notes = this.notes.filter(currentNote => currentNote.id != idToDelete)
     )
-  }
+  };
 
   ngOnInit(): void {
     this.notesService.getData().subscribe(resultsNotes => {
