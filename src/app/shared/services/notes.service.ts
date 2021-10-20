01a708d64d4ad9a93f7b8650e53b976fdfb44dd3
Lea-Vangheluwe <lv.vangheluwe@gmail.com>
2021-10-20 15:17:49 +0200

    connexion au serveur et récupération des notes ok

---
+++ src/app/shared/services/notes.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Note } from '../interfaces/noteInterface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotesService {
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
+
+notes: Note[] = [];
+
+  getData(): Observable<Note[]> {
+      return this.httpClient.get<Note[]>(`${environment.serverUrl}/notes/`);
+      console.log('passage dans le service')
+    }
+
+ //Suppression d'une note
+  deleteData(idToDelete: number){
+    let url = `${environment.serverUrl}/notes/${idToDelete}`;
+    return this.httpClient.delete(url, this.httpOptions)
+  }
+
+  addData(note: Note): Observable<Note> {
+      return this.httpClient.post<Note>(`${environment.serverUrl}/notes/`, note, this.httpOptions)
+  }
+
+  editNote(note: Note){
+    return this.httpClient.put<Note>(`${environment.serverUrl}/notes/${note.id}`, note, this.httpOptions)
+  }
+
+  constructor(private httpClient: HttpClient) { }
+}
